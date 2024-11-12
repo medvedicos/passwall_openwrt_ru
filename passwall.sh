8a35a3815ae1ff775c926907f2e82670d5a17a1f
medvedicos <41338299+medvedicos@users.noreply.github.com>
2024-11-12 11:02:13 +0300

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -55,7 +55,7 @@ opkg update
 
 ### Add Src ###
 
-wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+wget -O passwall.pub https://sourceforge.net/project/openwrt-passwall-build/passwall.pub
 
 opkg-key add passwall.pub
 
@@ -65,7 +65,7 @@ read release arch << EOF
 $(. /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
 EOF
 for feed in passwall_luci passwall_packages passwall2; do
-  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
+  echo "src/gz $feed https://sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
 done
 
 ### Install package ###
